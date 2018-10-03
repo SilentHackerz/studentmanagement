7d17a7b3bb035e72aab3c91f3bc067f9828956e1
SilentHackerz <ayubrazazain@gmail.com>
2018-10-03 22:25:01 +0530

    addeddatabase uri

--- config/secret.js
+++ config/secret.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://root:abc123@ds121345.mlab.com:21345/student',
+  database: 'mongodb://ayub:abc123@ds121299.mlab.com:21299/studentapp',
   secret: "hahahaha88888"
 }
